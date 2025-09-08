0f58e197a043d2f0d5d0da9f3ca7d6825ff7a1e6
hali-leaptools <hali@leaptools.com>
2025-09-08 12:23:33 -0400

    Configure Image for Pixabay images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
+  images: { remotePatterns: [new URL("https://cdn.pixabay.com/photo/**")] },
 };
 
 export default nextConfig;
