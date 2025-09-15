212b3657afee214b7b393f011472cd55c822bb05
hali-leaptools <hali@leaptools.com>
2025-09-15 14:07:29 -0400

    Add markdown linting

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,11 @@
-export default {
-    "*/**/*.{mjs,js,jsx,ts,tsx}": [
-        "eslint --fix",
-        "eslint"
-    ],
-    "*/**/*.{json,css,md}": [
-        "prettier --write"
-    ]
-}
+const config = {
+  "*.{mjs,js,jsx,ts,tsx}": (stagedFiles) => [
+    `eslint --fix ${stagedFiles.join(" ")}`,
+  ],
+  "*.{json,css}": (stagedFiles) => [
+    `prettier --write ${stagedFiles.join(" ")}`,
+  ],
+  "*.md": (stagedFiles) => [`markdownlint-cli2 --fix ${stagedFiles.join(" ")}`],
+};
+
+export default config;
