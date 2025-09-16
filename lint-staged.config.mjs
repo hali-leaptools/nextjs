a1e2f223b164e681a4914383f6ec2f3598376c4d
hali-leaptools <hali@leaptools.com>
2025-09-16 12:57:33 -0400

    Simplify lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,11 +1,7 @@
 const config = {
-  "*.{mjs,js,jsx,ts,tsx}": (stagedFiles) => [
-    `eslint --fix ${stagedFiles.join(" ")}`,
-  ],
-  "*.{json,css}": (stagedFiles) => [
-    `prettier --write ${stagedFiles.join(" ")}`,
-  ],
-  "*.md": (stagedFiles) => [`markdownlint-cli2 --fix ${stagedFiles.join(" ")}`],
+  "*.{mjs,js,jsx,ts,tsx}": ["eslint --fix"],
+  "*.{json,css}": ["prettier --write"],
+  "*.md": [`markdownlint-cli2 --fix`],
 };
 
 export default config;
